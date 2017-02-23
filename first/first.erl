ec5d5d3f110d106d022a70ada27bb3be08e75f7a
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-23 23:07:19 +0100

    add square

--- first/first.erl
+++ first/first.erl
@@ -1,23 +1,15 @@
 -module(first).
-
--export([double/1,mult/2,area/3]).
-
-
+-export([double/1,mult/2,area/3,square/1]).
 
 mult(X,Y) ->
-
     X*Y.
 
-
-
 double(X) ->
-
     mult(2,X).
 
-
-
 area(A,B,C) ->
-
     S = (A+B+C)/2,
-
     math:sqrt(S*(S-A)*(S-B)*(S-C)).
+
+square(X) ->
+    mult(X,X).
