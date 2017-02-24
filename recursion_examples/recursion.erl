0001501994a5b40e28ee4018caf4bf83c1ac0af3
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-24 21:38:18 +0100

    add step-by-step evaluation of fib(4)

--- recursion_examples/recursion.erl
+++ recursion_examples/recursion.erl
@@ -8,3 +8,9 @@ fib(1) ->
 fib(N) when N>1 ->
     fib(N-1) + fib(N-2).
 
+% fib(4)
+% fib(3) + fib(2)
+% (fib(2) + fib(1)) + (fib (1) + fib(0))
+% ((fib(1) + fib(0)) + fib (1)) + (fib(1) + fib(0))
+% ((1 + 0) +1) + (1 +0)
+% 3
