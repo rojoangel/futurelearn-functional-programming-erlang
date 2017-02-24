1f58662a62b011c48d5d0069a3bb44ed415bb39d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-24 23:15:15 +0100

    implement perfect/1 using tail recursion

--- tail_recursion/tail.erl
+++ tail_recursion/tail.erl
@@ -1,5 +1,5 @@
 -module(tail).
--export([fib/1]).
+-export([fib/1, perfect/1]).
 
 fib(0) ->
     0;
@@ -12,3 +12,15 @@ fib(0, PR, PRPR) ->
     PR + PRPR;
 fib(N, PR, PRPR) ->
     fib(N-1, PR + PRPR, PR).
+
+perfect(1) -> 
+    true;
+perfect(N) when N>1 ->
+    perfect(N, N-1, 0).
+
+perfect(N, 0, Sum) ->
+    N == Sum;
+perfect(N, Current, Sum) when N rem Current == 0 ->
+    perfect(N, Current-1, Current + Sum);
+perfect(N, Current, Sum)->
+    perfect(N, Current-1, Sum).
