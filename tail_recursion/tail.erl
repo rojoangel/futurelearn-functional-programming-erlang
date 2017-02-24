a05f239d2f52ab3d26fde2ffaab1104ff42ac3af
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-24 23:35:07 +0100

    refactor fib/1 tail recursion to a nicer solution

--- tail_recursion/tail.erl
+++ tail_recursion/tail.erl
@@ -1,17 +1,13 @@
 -module(tail).
 -export([fib/1, perfect/1]).
 
-fib(0) ->
-    0;
-fib(1) ->
-    1;
-fib(N) when N>1 ->
-    fib(N-2, 1, 0).    
+fib(N) ->
+    fib(N, 1, 0).    
 
-fib(0, PR, PRPR) ->
-    PR + PRPR;
-fib(N, PR, PRPR) ->
-    fib(N-1, PR + PRPR, PR).
+fib(0, _Cur, Pr) ->
+    Pr;
+fib(N, Cur, Pr) ->
+    fib(N-1, Cur + Pr, Cur).
 
 perfect(1) ->
     true;
