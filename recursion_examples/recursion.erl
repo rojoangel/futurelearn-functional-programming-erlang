10360955aa3a334f7fe30383cf8d5062df31bfb4
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-24 21:49:06 +0100

    implement pieces/1 using recursion

--- recursion_examples/recursion.erl
+++ recursion_examples/recursion.erl
@@ -1,5 +1,5 @@
 -module(recursion).
--export([fib/1]).
+-export([fib/1, pieces/1]).
 
 fib(0) -> 
     0;
@@ -14,3 +14,17 @@ fib(N) when N>1 ->
 % ((fib(1) + fib(0)) + fib (1)) + (fib(1) + fib(0))
 % ((1 + 0) +1) + (1 +0)
 % 3
+
+%% from http://www.math.toronto.edu/mccann/assignments/199S/cuttingplanes.pdf
+% pieces(0) -> 1;
+% pieces(1) -> 2;
+% pieces(2) -> 4; 
+% pieces(3) -> 7; 
+% pieces(4) -> 11;
+% pieces(5) -> 16;
+% pieces(6) -> 22;
+
+pieces(0) ->
+    1;
+pieces(N) when N>0 ->
+    pieces(N-1) + N.
