9319db15e3e500f95ca4522f5bdbdff3f2212b53
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-03 21:56:19 +0100

    add evens/1

--- w2/constructing_lists_with_recursive_functions/blists.erl
+++ w2/constructing_lists_with_recursive_functions/blists.erl
@@ -1,6 +1,17 @@
 -module(blists).
--export([double/1]).
+-export([double/1, evens/1]).
 
 % - double - direct recursion
 double([]) -> [];
 double([X|Xs]) -> [ 2 * X | double(Xs)].
+
+% - evens - direct recursion
+even(N) when (N rem 2) == 0 -> true;
+even(_) -> false.
+
+evens([]) -> [];
+evens([X|Xs]) -> 
+    case even(X) of
+        true -> [X|evens(Xs)];
+        _ -> evens(Xs)
+    end.
