13c937f993f05878c04e5f2668c222fe7825034d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-03 23:51:42 +0100

    implement median/1

--- w2/constructing_lists_with_recursive_functions/blists.erl
+++ w2/constructing_lists_with_recursive_functions/blists.erl
@@ -1,5 +1,5 @@
 -module(blists).
--export([double/1, evens/1]).
+-export([double/1, evens/1, median/1]).
 
 % - double - direct recursion
 double([]) -> [];
@@ -14,4 +14,39 @@ evens([X|Xs]) ->
     case even(X) of
         true -> [X|evens(Xs)];
         _ -> evens(Xs)
+    end.
+
+% - median - direct recursion
+
+partition(X,Ys) -> partition(X,Ys,[],[]). % call to tail recursive partition
+partition(_,[],Less,Greater) -> {Less, Greater}; % - tail recursive partition
+partition(X,[Y|Ys],Less,Greater) -> 
+    case Y < X of
+        true -> partition(X,Ys,[Y|Less],Greater);
+        _    -> partition(X,Ys,Less,[Y|Greater])
+    end.
+
+sort([]) -> []; % - quicksort
+sort([X|Xs]) -> 
+    {Less, Greater} = partition(X,Xs),
+    sort(Less) ++ [X] ++ sort(Greater).
+
+nth([_|_]=Xs,I) -> nth(Xs,I,0). % call to tail recursive nth
+nth([X|Xs],I,N) -> 
+    case I =:= N of
+        true -> X;
+        _    -> nth(Xs,I,N+1)
+    end.
+
+list_length(Xs) -> list_length(Xs,0). % call to tail recursive length
+list_length([],Length) -> Length; % - tail recursive length
+list_length([_|Xs],Length) -> list_length(Xs, Length + 1).
+
+median(Xs) ->
+    SortedXs = sort(Xs),
+    LengthXs = list_length(Xs),
+    MidPositionXs = LengthXs div 2,
+    case LengthXs rem 2 of
+        0 -> (nth(SortedXs,MidPositionXs-1) + nth(SortedXs,MidPositionXs)) / 2;
+        _ -> nth(SortedXs,MidPositionXs)
     end.
