8cf2ca3506b47a6398b301f9387d2a702b3670e8
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-04 21:49:17 +0100

    add take/2 and taketr/2 specs

--- w2/where_do_i_begin/wdib.erl
+++ w2/where_do_i_begin/wdib.erl
@@ -2,12 +2,14 @@
 -export([taketr/2, take/2]).
 
 % -- take tail recursive
+-spec taketr(integer(),[T]) -> [T].
 taketr(N,Xs) -> taketr(N,Xs,0,[]). % call to tail recursion implementation
 taketr(N,_Xs,N,Acc) -> Acc;
 taketr(_N,[],_M,Acc) -> Acc;
 taketr(N,[X|Xs],M,Acc) -> taketr(N,Xs,M+1,Acc++[X]).
 
 % -- take direct recursive
+-spec take(integer(), [T]) -> [T].
 take(_N,[]) -> [];
 take(0,_Xs) -> [];
 take(N,[X|Xs]) -> [X|take(N-1,Xs)].
