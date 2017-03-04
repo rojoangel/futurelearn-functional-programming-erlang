c62abff2fcb855c1fa791dd248ee6e0bd79e60bd
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-04 21:17:06 +0100

    add comments

--- w2/constructing_lists_with_recursive_functions/blists.erl
+++ w2/constructing_lists_with_recursive_functions/blists.erl
@@ -10,10 +10,11 @@ doubletr(Xs) -> doubletr(Xs,[]). % call to tail recursive doubletr
 doubletr([], Doubles) -> Doubles;
 doubletr([X|Xs], Doubles) -> doubletr(Xs, Doubles ++ [2 * X]).
 
-% - evens - direct recursion
+% - even helper function
 even(N) when (N rem 2) == 0 -> true;
 even(_) -> false.
 
+% - evens - direct recursion
 evens([]) -> [];
 evens([X|Xs]) -> 
     case even(X) of
@@ -30,8 +31,11 @@ evenstr([X|Xs], Evens) ->
         _    -> evenstr(Xs, Evens)
     end.
 
-% - median - direct recursion
+% --------
+% - median
+% --------
 
+% - partition helper function
 partition(X,Ys) -> partition(X,Ys,[],[]). % call to tail recursive partition
 partition(_,[],Less,Greater) -> {Less, Greater}; % - tail recursive partition
 partition(X,[Y|Ys],Less,Greater) -> 
@@ -40,11 +44,13 @@ partition(X,[Y|Ys],Less,Greater) ->
         _    -> partition(X,Ys,Less,[Y|Greater])
     end.
 
+% - sort helper function
 sort([]) -> []; % - quicksort
 sort([X|Xs]) -> 
     {Less, Greater} = partition(X,Xs),
     sort(Less) ++ [X] ++ sort(Greater).
 
+% - nth helper function
 nth([_|_]=Xs,I) -> nth(Xs,I,0). % call to tail recursive nth
 nth([X|Xs],I,N) -> 
     case I =:= N of
@@ -52,10 +58,12 @@ nth([X|Xs],I,N) ->
         _    -> nth(Xs,I,N+1)
     end.
 
+% - list_length helper function
 list_length(Xs) -> list_length(Xs,0). % call to tail recursive length
 list_length([],Length) -> Length; % - tail recursive length
 list_length([_|Xs],Length) -> list_length(Xs, Length + 1).
 
+% - median
 median(Xs) ->
     SortedXs = sort(Xs),
     LengthXs = list_length(Xs),
@@ -65,8 +73,11 @@ median(Xs) ->
         _ -> nth(SortedXs,MidPositionXs)
     end.
 
-% - modes - direct recursion
+% -------
+% - modes
+% -------
 
+% - count & count_sorted helper function
 count(Xs) -> count_sorted(sort(Xs)).
 
 count_sorted([X|Xs]) -> count_sorted(Xs, X, 1, []). % call to tail recursive count_sorted
@@ -74,10 +85,12 @@ count_sorted([], CurrentVal , CurrentCount, Counts) -> [{CurrentVal, CurrentCoun
 count_sorted([X|Xs], X, CurrentCount, Counts) -> count_sorted(Xs, X, CurrentCount + 1, Counts);
 count_sorted([X|Xs], Y, CurrentCount, Counts) -> count_sorted(Xs, X, 1, [{Y, CurrentCount}|Counts]).
 
+% - max_counts helper function
 max_counts([{Val, Count}|Xs]) -> max_counts(Xs, [Val], Count). % call to tail recursive max_counts
 max_counts([], Modes, _MaxCount) -> Modes;
 max_counts([{Val, Count}|Xs], Modes, Count) -> max_counts(Xs, [Val|Modes], Count);
 max_counts([{Val, Count}|Xs], _Modes, MaxCount) when Count > MaxCount -> max_counts(Xs, [Val], Count);
 max_counts([{_Val, _Count}|Xs], Modes, MaxCount) -> max_counts(Xs, Modes, MaxCount).
 
+% - modes
 modes(Xs) -> max_counts(count(Xs)).
