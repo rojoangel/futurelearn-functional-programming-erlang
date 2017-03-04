aa145c09a75732cf726a7477fdd36e6af9941d29
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-04 22:17:23 +0100

    add taketr2/2 - a less convoluted tail recursive solution leveraging lists:reverse

--- w2/where_do_i_begin/wdib.erl
+++ w2/where_do_i_begin/wdib.erl
@@ -1,5 +1,11 @@
 -module(wdib).
--export([taketr/2, take/2]).
+-export([take/2, taketr/2, taketr2/2]).
+
+% -- take direct recursive
+-spec take(integer(), [T]) -> [T].
+take(_N,[]) -> [];
+take(0,_Xs) -> [];
+take(N,[X|Xs]) when N>0-> [X|take(N-1,Xs)].
 
 % -- take tail recursive
 -spec taketr(integer(),[T]) -> [T].
@@ -8,8 +14,9 @@ taketr(N,_Xs,N,Acc) -> Acc;
 taketr(_N,[],_M,Acc) -> Acc;
 taketr(N,[X|Xs],M,Acc) -> taketr(N,Xs,M+1,Acc++[X]).
 
-% -- take direct recursive
--spec take(integer(), [T]) -> [T].
-take(_N,[]) -> [];
-take(0,_Xs) -> [];
-take(N,[X|Xs]) when N>0-> [X|take(N-1,Xs)].
+% -- take less convoluted tail recursive
+-spec taketr2(integer(),[T]) -> [T].
+taketr2(N,Xs) -> taketr2(N,Xs,[]). % call to tail recursion implementation
+taketr2(0,_Xs,Acc) -> lists:reverse(Acc);
+taketr2(_N,[],Acc) -> lists:reverse(Acc);
+taketr2(N,[X|Xs],Acc) -> taketr2(N-1, Xs, [X|Acc]).
