5ffe5e9b6eb4099eb9cdc5c0295d4d78bdd7dfe9
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-07 21:35:30 +0100

    define product using foldr and a lambda

--- w3/higher_order_functions_in_practice/hofs.erl
+++ w3/higher_order_functions_in_practice/hofs.erl
@@ -1,5 +1,5 @@
 -module(hofs).
--export([doubleAll/1,evens/1]).
+-export([doubleAll/1,evens/1,product/1]).
 
 % - double all
 %
@@ -20,3 +20,13 @@ doubleAll(Xs) ->
 
 evens(Xs) -> 
     lists:filter(fun (X) -> X rem 2 == 0 end,Xs).
+
+% - product
+%
+% 8> hofs:product([]).
+% 1
+% 9> hofs:product([1,2,3,4,5,6,7,8,9]).
+% 362880
+
+product(Xs) ->
+    lists:foldr(fun (X,Y) -> X * Y end , 1, Xs).
